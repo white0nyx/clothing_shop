79b792abad353622ca7bdfc4855434efff6b3115
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2023-02-25 16:31:16 +0300

    Связан разработанный frontend и backend. На данный момент реализованы главная страница, страницы категорий, входа и регистрации

--- clothing_store/shop/urls.py
+++ clothing_store/shop/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from shop.views import *
 
 urlpatterns = [
-    path('', home_page, name='home'),
+    path('', HomePage.as_view(), name='home'),
     path('registration/', RegistrationPage.as_view(), name='registration'),
     path('login/', LoginPage.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
--- clothing_store/shop/views.py
+++ clothing_store/shop/views.py
@@ -1,19 +1,45 @@
+import math
+
 from django.contrib.auth import logout, login
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.views import LoginView
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, _get_queryset
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, DetailView
 
 from shop.forms import RegisterUserForm, LoginUserForm
 from shop.models import *
 
+ITEMS_IN_LINE = 3
+
 
 def home_page(request):
     """Функция представления главной страницы"""
 
     return render(request, 'shop/home.html', context={'title': 'Главная страница'})
+
+
+def split_list_into_chunks(items):
+    chunk_size = ITEMS_IN_LINE
+    return [items[i:i + chunk_size] for i in range(0, len(items), chunk_size)]
+
+
+class HomePage(ListView):
+    """Класс представления главной страницы"""
+
+    model = Item
+    template_name = 'shop/category_page.html'
+    context_object_name = 'items'
+
+    def get_queryset(self):
+        items = Item.objects.all()
+        return split_list_into_chunks((list(items)))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'WearFit'
+        return context
 
 
 def logout_user(request):
@@ -29,7 +55,8 @@ class CategoryPage(ListView):
     context_object_name = 'items'
 
     def get_queryset(self):
-        return Item.objects.filter(category__slug=self.kwargs['category_slug'], is_in_stock=True)
+        items = Item.objects.filter(category__slug=self.kwargs['category_slug'], is_in_stock=True)
+        return split_list_into_chunks((list(items)))
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
