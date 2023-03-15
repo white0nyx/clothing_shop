a3ce421588671ebb5e7a238175e924bd2b2c5668
IlonMaxi <elfimov.maksim1415@gmail.com>
2023-03-15 22:54:45 +0300

    Работающий счётчик

--- clothing_store/shop/static/shop/js/schet.js
+++ clothing_store/shop/static/shop/js/schet.js
@@ -1,29 +1,32 @@
 const counters = document.querySelectorAll('[data-counter]');
 
-if (counters){
-    counters.forEach(counter => {
-        counter.addEventListener('click',e => {
-            const target = e.target;
+if (counters) {
+	counters.forEach(counter => {
+		counter.addEventListener('click', e => {
+			const target = e.target;
 
-            if (target.closest('.counter_button')){
-                let value = parseInt(target.closest('.counter').querySelector('input').value);
+			if (target.closest('.counter_button')) {
+				if (target.closest('.counter').querySelector('input').value == '' && (target.classList.contains('counter_button_minus') || target.classList.contains('counter_button_plus'))) {
+					target.closest('.counter').querySelector('input').value = 0;
+				}
 
-                if(target.classList.contains('counter_button_plus')){
-                    value++;
-                }
-                else{
-                    --value;
-                }
+				let value = parseInt(target.closest('.counter').querySelector('input').value);
 
-                if (value <= 0){
-                    value = 0;
-                    target.closest('.counter').querySelector('counter_button_minus').classList.add('disabled')
-                }
-                else{
-                    target.closest('.counter').querySelector('counter_button_minus').classList.remove('disabled')
-                }
-                target.closest('.counter').querySelector('input').value = value;
-            }
-        })
-    })
+				if (target.classList.contains('counter_button_plus')) {
+					value++;
+				} else {
+					--value;
+				}
+
+				if (value <= 0) {
+					value = 0;
+					target.closest('.counter').querySelector('.counter_button_minus').classList.add('disabled')
+				} else {
+					target.closest('.counter').querySelector('.counter_button_minus').classList.remove('disabled')
+				}
+
+				target.closest('.counter').querySelector('input').value = value;
+			}
+		})
+	})
 }
