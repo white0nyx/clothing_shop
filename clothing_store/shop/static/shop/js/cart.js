39455f104fb38ab32075ddfe95a656ef89dac715
DFimp <118912553+DFimp@users.noreply.github.com>
2023-03-21 21:11:22 +0300

    Исправил совмещение коммитов

--- clothing_store/shop/static/shop/js/cart.js
+++ clothing_store/shop/static/shop/js/cart.js
@@ -1,8 +1,8 @@
 $('.header-buttons button').on('click', function () {
-                        $('.popups-inner-basket').addClass('active');
+                    $('.modal__cart.modal').addClass('active');
 
-                    });
-                    $('.close-popup-basket').on('click', function () {
-                        $('.popups-inner-basket').removeClass('active');
+                });
+                $('.modal__cart-continue-shopping-2 button').on('click', function () {
+                    $('.modal__cart.modal').removeClass('active');
 
-                    });
+                });
