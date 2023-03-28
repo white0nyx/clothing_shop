c89bb0df1db8aa6b92fb5e1f0142e58a9dcfca30
Matvey Sherbaev <sherbaev.matvey@mail.ru>
2023-03-28 22:25:13 +0300

    сделана регистрация через форму

--- clothing_store/shop/forms.py
+++ clothing_store/shop/forms.py
@@ -9,11 +9,43 @@ from shop.models import User
 class RegisterUserForm(UserCreationForm):
     """Форма регистрации пользователя"""
 
-    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    email = forms.CharField(label='Почта', widget=forms.EmailInput(attrs={'class': 'form-input'}))
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    password2 = forms.CharField(label='Подтверждение пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    captcha = CaptchaField(label='')
+    # username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    # username = forms.CharField(label='Почта', widget=forms.EmailInput(attrs={'class': 'form-input'}))
+    # password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    # password2 = forms.CharField(label='Подтверждение пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    # captcha = CaptchaField(label='')
+
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={
+        'class': 'modal__signup-classic-field-input',
+        'id': 'signup-username',
+        'maxlength': "255",
+        'autocomplete': 'username',
+        'required': True,
+    }))
+
+    email = forms.CharField(label='Эл.почта', widget=forms.EmailInput(attrs={
+        'class': 'modal__signup-classic-field-input',
+        'id': 'signup-email',
+        'maxlength': "255",
+        'autocomplete': 'email',
+        'required': True,
+    }))
+
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={
+        'class': 'modal__signup-classic-field-input',
+        'id': 'signup-password',
+        'maxlength': "255",
+        'autocomplete': 'new-password',
+        'required': True,
+    }))
+
+    password2 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={
+        'class': 'modal__signup-classic-field-input',
+        'id': 'signup-confirmation',
+        'maxlength': "255",
+        'autocomplete': 'new-password',
+        'required': True,
+    }))
 
     class Meta:
         model = User
@@ -23,7 +55,6 @@ class RegisterUserForm(UserCreationForm):
 class LoginUserForm(AuthenticationForm):
     """Форма авторизации пользователя"""
 
-    # password = forms.CharField(label='ПАРОЛЬ', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
     username = forms.EmailField(label='ЭЛ.ПОЧТА', widget=forms.EmailInput(attrs={
             'class': 'modal__signin-classic-field-input',
