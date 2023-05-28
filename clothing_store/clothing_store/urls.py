b4d9bf45bd119ff5d5eb1a0e73161d5f0eec35bc
Matvey Sherbaev <sherbaev.matvey@mail.ru>
2023-05-28 14:49:42 +0300

    удалена капча

--- clothing_store/clothing_store/urls.py
+++ clothing_store/clothing_store/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('captcha/', include('captcha.urls')),
     path('', include('shop.urls')),
 ]
 
