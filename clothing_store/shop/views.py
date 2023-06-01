727cffb9c3695a4f5e93210aaf2e675a39b315bb
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2023-06-01 20:29:42 +0300

    Исправляем козину

--- clothing_store/shop/views.py
+++ clothing_store/shop/views.py
@@ -231,7 +231,7 @@ def cart(request):
 def cart_add(request, item_id):
 
     if request.user.pk is None:
-        return redirect('home')
+        return redirect('registration')
 
     item = get_object_or_404(Item, id=item_id)
     size = request.POST['size']
